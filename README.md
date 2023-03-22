af351c3ba3c213082b7f907f29a7980b4bb40a8b
Andrew Renninger <awrenninger@crimson.ua.edu>
2023-03-21 22:12:29 -0500

    readme and test

---
+++ README.md
@@ -0,0 +1,5 @@
+# webhookLog
+A simple package for sending log messages to discord webhooks
+
+###### Install
+`go get github.com/abearinatrap/webhookLog`
--- test.go
+++ test.go
@@ -1,10 +1,17 @@
 package webhookLog
 
+import (
+	"fmt"
+	"time"
+)
+
 var Log *DefaultLogger
 
 func main() {
 	// test logging
-	Log = NewDefaultLogger("test", "")
-	Log.Infof("test")
-	Log.Errorf("yo")
+	Log = NewDefaultLogger("test", "webhookkey here")
+	start := time.Now()
+	Log.Infof("Testing log")
+	duration := time.Since(start)
+	fmt.Printf("Time to send %v\n", duration)
 }
