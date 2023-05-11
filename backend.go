d253d3e63640108fcd3ddc5489012281b9a4507f
Andrew Renninger <awrenninger@crimson.ua.edu>
2023-05-11 18:48:39 -0400

    bounded execution no longer sends empty

--- backend.go
+++ backend.go
@@ -9,14 +9,20 @@ func (l *DefaultLogger) runBack() {
 	l.queue = make(chan string, l.queueSize)
 	message := ""
 	fmt.Printf("Backend logger running")
+	waitTime := 1000
 	for {
 		//bounded execution time for log sending
 		select {
 		case newMessage := <-l.queue:
 			message += string(newMessage)
 			message, _ = l.sendMessage(message)
-		case <-time.After(1 * time.Second):
-			message, _ = l.sendMessage(message)
+		case <-time.After(time.Duration(waitTime) * time.Millisecond):
+			if len(message) > 0 {
+				message, _ = l.sendMessage(message)
+				waitTime = 250
+			} else {
+				waitTime = 1000
+			}
 		}
 
 	}
